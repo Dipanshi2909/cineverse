3b6604260622bb4260254efe2d09cb50217d9f44
Deepanshi <Dipanshi299@gmail.com>
2026-06-11 12:15:56 +0530

    Netflix UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieCatalog from "./pages/MovieCatalog";
 import Watchlist from "./pages/Watchlist";
+import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<MovieCatalog />} />
         <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,26 +1,36 @@
+import { Link } from "react-router-dom";
+
 function MovieCard({ title, rating, poster }) {
   return (
-    <div
+    <Link
+      to="/movie/1"
       style={{
-        width: "220px",
-        backgroundColor: "#222",
-        borderRadius: "10px",
-        overflow: "hidden",
-        transition: "0.3s",
+        textDecoration: "none",
+        color: "inherit",
       }}
     >
-      <img
-        src={poster}
-        alt={title}
-        width="100%"
-        height="300"
-      />
+      <div
+        style={{
+          width: "220px",
+          backgroundColor: "#222",
+          borderRadius: "10px",
+          overflow: "hidden",
+          transition: "0.3s",
+        }}
+      >
+        <img
+          src={poster}
+          alt={title}
+          width="100%"
+          height="300"
+        />
 
-      <div style={{ padding: "10px", color: "white" }}>
-        <h3>{title}</h3>
-        <p>⭐ {rating}</p>
+        <div style={{ padding: "10px", color: "white" }}>
+          <h3>{title}</h3>
+          <p>⭐ {rating}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
